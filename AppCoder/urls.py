94992ab6972ce0e33fbbb551b9eeb754beacb39b
German Pereira <german.pereira03@gmail.com>
2023-11-19 20:48:28 -0300

    first commit

---
+++ AppCoder/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from AppCoder.views import crear_curso, show_html, mostrar_cursos
+
+urlpatterns = [
+    path('agregar_curso/', crear_curso),
+    path('show/', show_html),
+    path('cursos/', mostrar_cursos),
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from AppCoder.models import Curso
 
-# Create your views here.
+
+def mostrar_cursos(request):
+    cursos = Curso.objects.all()
+    contexto = {
+        "cursos": cursos,
+        "nombre": "German"
+    }
+    return render(request, "AppCoder/cursos.html", contexto)
+
+
+def crear_curso(request):
+    curso = Curso(nombre ="Python", camada=41000)
+    curso.save()
+
+    contexto = {"curso": curso}
+
+    return redirect("/app/cursos/") # get
+    #return render(request, 'index.html', contexto)
+    #return HttpResponse(f"Su curso es {curso.nombre} y la camada es {curso.camada}")
+
+def show_html(request):
+    curso = Curso.objects.first()
+    contexto = {"curso": curso, "nombre": "German"}
+    return render(request, 'index.html', contexto)
